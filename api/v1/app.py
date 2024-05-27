9d9900a8ca08d92f56863a90e184b3c2f7da9941
0obito <elmsafer.abdellah00@gmail.com>
2024-05-27 12:33:46 +0100

    made some tiny modifications here and there to the code, hoping for the checkers to work :)

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,22 +3,20 @@
 A module that sets up a Flask application
 with a specific configuration
 """
-from flask import Flask
+from flask import Flask, Blueprint, jsonify
 from models import storage
 from api.v1.views import app_views
-from os import getenv
+import os
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
-
+app.register_blueprint(app_views, url_prefix="/api/v1")
+host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+port = int(os.getenv('HBNB_API_PORT', '5000'))
 
 @app.teardown_appcontext
-def teardown():
+def teardown(code):
     """Closes the storage on teardown"""
     storage.close()
 
-
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = getenv('HBNB_API_PORT', '5000')
-    app.run(host=host, port=int(port), threaded=True)
+    app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,8 +5,10 @@ This module defines routes for API
 from api.v1.views import app_views
 from flask import jsonify
 
-
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False)
 def status():
     """ Provides the status of the API """
     return jsonify({"status": "OK"})
+
+if __name__ == "__main__":
+    pass
